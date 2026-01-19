06f393589b61722f5037738d5623f74c474aa9b0
ismelen <ismael.ma34k@gmail.com>
2026-01-19 22:39:02 +0100

    feat: new icons

--- src/theme/icons.tsx
+++ src/theme/icons.tsx
@@ -76,3 +76,10 @@ export function FolderZipIcon({ size, color }: Props) {
     </Svg>
   );
 }
+export function ScheduleIcon({ size, color }: Props) {
+  return (
+    <Svg height={size} viewBox="0 -960 960 960" width={size} fill={color}>
+      <Path d="m612-292 56-56-148-148v-184h-80v216l172 172ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Z" />
+    </Svg>
+  );
+}
