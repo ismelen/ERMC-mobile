bb60072f7c61165b286202e9dfb1750ce3bf7fc2
ismelen <ismael.ma34k@gmail.com>
2026-02-07 17:03:55 +0100

    refactor: new colors

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -1,12 +1,8 @@
 export const colors = {
-  primary: '#2bbeee',
-  background: '#f6f8f8',
-  white: '#ffffff',
-  iconDisabled: '#4c869a',
-  border: '#f1f5f9',
-  textMuted: '#787280',
-  cardMuted: '#f8fbfb',
-  textGray: '#aaa8ae',
-} as const;
-
-export type ColorKey = keyof typeof colors;
+  primary: '#3a83f6',
+  white: '#fefffe',
+  background: '#121313',
+  card: '#1d1c1f',
+  onCard: '#8e8e93',
+  gray: '#39383c',
+};
